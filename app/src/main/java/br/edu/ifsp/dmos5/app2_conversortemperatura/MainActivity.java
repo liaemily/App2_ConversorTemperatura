fe2400baa3b2eea1284a55fc5cb673bfcbc3843d
Lia Emily Cavalheiro <liaemily2001@gmail.com>
2023-03-05 23:03:15 -0300

    Versão 4

--- app/src/main/java/br/edu/ifsp/dmos5/app2_conversortemperatura/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/dmos5/app2_conversortemperatura/MainActivity.java
@@ -2,10 +2,13 @@ package br.edu.ifsp.dmos5.app2_conversortemperatura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,15 +16,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private FahrenheitStrategy FahrenheitConversion = new FahrenheitStrategy();
     private CelsiusStrategy CelsiusConversion = new CelsiusStrategy();
-
     private KelvinStrategy KelvinConversion = new KelvinStrategy();
     private EditText inputEditText;
-    private Button converterCelsiusButton;
-    private Button converterFahrenheitButton;
-    private Button converterKelvinButton;
+
+    private Spinner spinner;
+    private Button converterButton;
     private TextView outputTextView;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,27 +33,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //associar atributos XML com os objetos Java
 
         inputEditText = findViewById(R.id.edittext_input);
-        converterCelsiusButton = findViewById(R.id.button_converter_celsius);
-        converterFahrenheitButton = findViewById(R.id.button_converter_fahrenheit);
-        converterKelvinButton = findViewById(R.id.button_converter_kelvin);
+        spinner = findViewById(R.id.spin_escala);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.escala_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        converterButton = findViewById(R.id.button_converter);
         outputTextView = findViewById(R.id.textview_output);
-        converterCelsiusButton.setOnClickListener(this);
-        converterFahrenheitButton.setOnClickListener(this);
-        converterKelvinButton.setOnClickListener(this);
+        converterButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == converterCelsiusButton){
-            getCelsiusConversion();
-        }
+        if (v == converterButton) {
 
-        else if (v == converterFahrenheitButton){
-            getFahrenheitConversion();
-        }
+            String escala = spinner.getSelectedItem().toString();
 
-        else if (v == converterKelvinButton){
-            getKelvinConversion();
+            if (escala.equals("Celsius")) {
+                getCelsiusConversion();
+            } else if (escala.equals("Fahrenheit")) {
+                getFahrenheitConversion();
+            } else {
+                getKelvinConversion();
+            }
+
         }
     }
 
